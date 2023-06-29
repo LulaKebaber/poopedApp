518c4947175a607a371dda06057cc9ca9b6517ad
Danial <danial-baizak@mail.ru>
2023-06-29 15:52:25 +0600

    Initial Commit

---
+++ poopedApp/poopedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  poopedAppApp.swift
+//  poopedApp
+//
+//  Created by Danial Baizak on 29.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct poopedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
